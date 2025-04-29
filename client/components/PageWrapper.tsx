ed30498e284928f1dd860d96687db8c921fc447b
Val Letigo <valletigio54@gmail.com>
2025-04-29 09:57:30 +0800

    upload button and image display
    successful build

---
+++ client/components/PageWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { HTMLMotionProps, motion } from "framer-motion";
+
+const PageWrapper = (props: HTMLMotionProps<"div">) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1 }}
+      {...props}
+    />
+  );
+};
+
+export default PageWrapper;
