b1f807184eae7b075469b086581214bb0a1ca2b9
Val Letigo <valletigio54@gmail.com>
2025-05-06 13:40:43 +0800

    yes yes

--- client/app/api/Server.ts
+++ client/app/api/Server.ts
@@ -6,6 +6,7 @@ class Server {
     constructor() {
         const next_env = process.env.NODE_ENV || 'development';
         const urls = {
+            // localApi: 'http://192.168.1.90:5000',
             localApi: 'http://127.0.0.1:5000',
             productionApi: null,
         };
