1604f0c6ac208447be7fe663c2b7af4e4ef12fa1
Val Letigo <valletigio54@gmail.com>
2025-05-08 15:30:46 +0800

    .

--- client/components/ThemeControl.tsx
+++ client/components/ThemeControl.tsx
@@ -6,7 +6,7 @@ const ThemeControl = () => {
       <input type="checkbox" className="theme-controller" value="capagain" />
 
       <svg
-        className="swap-off h-10 w-10 fill-current"
+        className="swap-off h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -14,7 +14,7 @@ const ThemeControl = () => {
       </svg>
 
       <svg
-        className="swap-on h-10 w-10 fill-current"
+        className="swap-on h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
