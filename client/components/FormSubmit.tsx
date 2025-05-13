6fc397a4d644068c9f21fe31338d8df5efe46d57
Val Letigo <valletigio54@gmail.com>
2025-05-13 14:26:24 +0800

    .

--- client/components/FormSubmit.tsx
+++ client/components/FormSubmit.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import ProgressLoading from "./ProgressLoading";
+
 interface FormSubmitProps {
   data: {
     [key: string]: unknown;
@@ -9,22 +11,33 @@ interface FormSubmitProps {
 const FormSubmit: React.FC<FormSubmitProps> = ({ data }) => {
   // const [formData, setFormData] = React.useState<FormData | null>(null);s
 
+  const [loading, setLoading] = React.useState(false);
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+    setLoading(true);
 
     fetch(
       "https://script.google.com/macros/s/AKfycbz7_7VGL8iAzRKhHTmx8zfBIEcoI_iQLevi-kpZwZvBxH_YkyvuXbpB9E5I4kMsROsqdw/exec",
       { method: "POST", body: formData }
     )
       .then((response) => alert("Thank you! Form is submitted"))
-      .catch((error) => console.error("Error!", error.message));
+      .catch((error) => console.error("Error!", error.message))
+      .finally(() => {
+        setLoading(false);
+        window.location.reload();
+      });
   };
   return (
     <form
       onSubmit={handleSubmit}
       className="bg-base-100 flex justify-center rounded-box "
-    > 
+    >
+      <ProgressLoading
+        loading={loading}
+        progressData={{ progress: 0, msg: "Submition on Progress" }}
+      />
       <fieldset className=" fieldset rounded-box place-content-evenly border p-4 w-full">
         <legend className="fieldset-legend">Spreadsheet Submition</legend>
 
