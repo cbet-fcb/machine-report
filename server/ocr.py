40902d70893e11c675b6c18988deb287ed3ece0a
cbet-fcb <famechristopherbaltazar@gmail.com>
2025-05-14 10:56:50 +0800

    run_ocr debugging

--- server/ocr.py
+++ server/ocr.py
@@ -36,16 +36,18 @@ class OCREngine:
         ocr_res = self.ocr_engine.ocr(image_array, cls=True)
         return self.process_ocr_output(ocr_res)
 
-    def process_ocr_output(self, ocr_result: list) -> str:
+    def process_ocr_output(self, ocr_result: list) -> str | None:
         """
         Process the raw OCR output to group text by lines.
         """
-        grouped_lines = self.group_text_lines(ocr_result)
+        if ocr_result:
+            grouped_lines = self.group_text_lines(ocr_result)
         
-        # For example, join the text for each line
-        grouped_text = " ".join([" ".join([word["text"] for word in line["words"]]) for line in grouped_lines])
+            # For example, join the text for each line
+            grouped_text = " ".join([" ".join([word["text"] for word in line["words"]]) for line in grouped_lines])
+            return grouped_text     
         
-        return grouped_text
+        return None
 
     def compute_angle(self, box):
         (x1, y1), (x2, y2) = box[0], box[3]
